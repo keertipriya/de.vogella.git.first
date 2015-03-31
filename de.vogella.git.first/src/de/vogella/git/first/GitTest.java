4af97fb2d5876e3fa6947885877fef35453e5239
ADMIN <ADMIN@keertipriya>
2015-03-31 17:21:55 +0530

    test commit

--- de.vogella.git.first/src/de/vogella/git/first/GitTest.java
+++ de.vogella.git.first/src/de/vogella/git/first/GitTest.java
@@ -3,6 +3,7 @@ package de.vogella.git.first;
 public class GitTest {
 	  public static void main(String[] args) {
 	    System.out.println("Git is fun");
-	    
+	    System.out.println("master commit 3");
+	    System.out.println("master commit 4");
 	  }
 	} 
